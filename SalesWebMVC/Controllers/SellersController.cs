9b8f90d5bf1293790390f776f27628ab70e03608
Denis Tadeu Andre <denistandre@gmail.com>
2019-12-20 17:50:59 -0300

    Simple Create form

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
@@ -15,6 +16,21 @@ namespace SalesWebMVC.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // HttpPost - Para indicar uma acao de POST
+        // ValidateAntiForgeryToken - para prevenir que a aplicacao sofra ataque csrf - quando alguem envia dados maliciosos aproveitando a sua autenticacao
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
